22b9ddecc2f52df41cd0b90a6d41e498ce791045
Ahmed AL-OBEIDI <alobeidi.dev@gmail.com>
2025-10-05 21:38:53 +0200

    ADD: plants and user_plant tables

---
+++ database/migrations/2025_10_05_191429_create_plants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plants', function (Blueprint $table) {
+            $table->id();
+            $table->string('common_name');
+            $table->json('watering_general_benchmark');
+            $table->timestamps();
+        });
+
+        // Table pivot pour la relation Many-to-Many entre User et Plant
+        Schema::create('user_plant', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('plant_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_plant');
+        Schema::dropIfExists('plants');
+    }
+};
